ae82357997d34e819d9c9303ef7d29820674da7a
Ayo Ajayi <ayomide.ajayi2839@gmail.com>
2021-10-03 00:43:33 -0400

    Design homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import './Calendar.js';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div>
-      <Calendar/>
-    </div>
-  );
+    <Wrapper>
+      <Wrapper2>   <Calendar/> </Wrapper2>
+    
+      <p>happy</p>
+      <Button>SUBMIT MEEE</Button>
+    </Wrapper>
+  )
 }
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify_content: center;
+  padding: 4em;
+  height: 100vh;
+  background: papayawhip;
+`;
+const Wrapper2 = styled.section`
+  
+ 
+  padding: 4em;
+  background: black;
+
+`;
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+`
 
 export default App;

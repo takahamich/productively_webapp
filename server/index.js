64a42335588d8abd67a3fc9fd10b6adfc502f210
Nolan Lombardo <nolanlombardo@Nolans-MacBook-Air.local>
2021-10-19 02:42:46 -0400

    rough framework for mongodb

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Router = require("./DB/routes");
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,6 +9,18 @@ const app = express();
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
 });
+
+mongoose.connect(
+    `mongodb+srv://nolombardo:%40ndw3simplys%40id@cluster0.kjv7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+app.use(Router);
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);

8862fe0c43257e48c195a068b5c63e1e0e52c28e
Keidai Lee <lee.keidai@outlook.com>
2021-10-28 18:01:14 -0400

    New get and post endpoints HOORAAAYYY

--- server/routes.js
+++ server/routes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const taskModel = require("./models/Task");
-const UserModel = require("./models/User");
+const userModel = require("./models/User");
 const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.post("/add_task", async (req, res) => {
-    const task = new taskModel(req.body);
+// TODO: send message if x not found, instead of throwing an error?
+
+app.get("/users", async (req, res) => {
+    const users = await userModel.find({});
 
     try {
-        await task.save();
-        res.send(task);
+        res.send(users);
     } catch (error) {
         res.status(500).send(error);
     }
@@ -26,5 +27,43 @@ app.get("/tasks", async (req, res) => {
         res.status(500).send(error);
     }
 });
+
+app.get("/tasks/{id}", async (req, res) => {
+    const user = await userModel.find(req.params.id);
+    const tasks = new user.tasks;
+
+    // find within user?
+    try {
+        res.send(tasks);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.post("/tasks/{id}", async (req, res) => {
+    const user = new userModel(req.params.id);
+    user.tasks = user.tasks.append(req.body);
+    // check if req.body is formatted correctly
+
+    try {
+        await user.save();
+        res.send(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.post("/users", async (req, res) => {
+    const users = new userModel(req.body);
+
+    try {
+        await users.save();
+        res.send(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// app post: task score algorithm
 
 module.exports = app;

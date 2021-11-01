af917fc12ebe2c0272fee243be7917410cdc4faa
Shaina Gabala <sgabala2@jh.edu>
2021-10-31 22:20:45 -0400

    still wokring on endpoints. slight progress, trying to understand errors

--- server/models/User.js
+++ server/models/User.js
@@ -1,13 +1,50 @@
+//const taskModel = require("./Task");
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-    username: {
+const TaskSchema = new mongoose.Schema({
+    _id: mongoose.Types.ObjectId,
+    taskName: {
         type: String,
         required: true,
     },
-    tasks: Array,
+    predictedEndDate: {
+        type: Date,
+        required: true,
+    },
+    priority: {
+        type: Number,
+        required: true,
+    },
+    startDate: {
+        type: Date,
+        default: Date.now,
+    },
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    __v: {
+        type: Number,
+        default: 0,
+    },
+});
+
+const UserSchema = new mongoose.Schema({
+    tasks: [TaskSchema], //might not be necessary to have .of(tasModel)
+    _id: mongoose.Types.ObjectId,
+    name: {
+        type: String,
+        required: true,
+    },
+    age: Number,
+    __v: {
+        type: Number,
+        default: 0,
+    },
 });
 
 const User = mongoose.model("User", UserSchema);
+//const Task = mongoose.model("Task", TaskSchema);
 
 module.exports = User;
+//module.exports = Task;
--- server/routes.js
+++ server/routes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const taskModel = require("./models/Task");
 const userModel = require("./models/User");
 const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(bodyParser.json());
 
 // TODO: send message if x not found, instead of throwing an error?
 
-app.get("/users", async (req, res) => {
+app.get("/users", async (req, res) => { //gets all users
     const users = await userModel.find({});
 
     try {
@@ -18,7 +19,7 @@ app.get("/users", async (req, res) => {
     }
 });
 
-app.get("/tasks", async (req, res) => {
+app.get("/tasks", async (req, res) => { //gets all tasks
     const tasks = await taskModel.find({});
 
     try {
@@ -28,25 +29,42 @@ app.get("/tasks", async (req, res) => {
     }
 });
 
-app.get("/tasks/{id}", async (req, res) => {
-    const user = await userModel.find(req.params.id);
-    const tasks = new user.tasks;
+app.get("/tasks/:id", async (req, res) => {
+    const user = await userModel.find({_id: req.params.id});
+    console.log(user); // works
+    //or const tasks = await taskModel.find({creator: req.params.id});
+    console.log(user.tasks);//user.tasks undefined
+    res.send(user); //taskArray
 
     // find within user?
-    try {
-        res.send(tasks);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+    // try {
+    //     res.send(tasks);
+    // } catch (error) {
+    //     res.status(500).send(error);
+    // }
 });
 
-app.post("/tasks/{id}", async (req, res) => {
-    const user = new userModel(req.params.id);
-    user.tasks = user.tasks.append(req.body);
+app.post("/tasks/:id", async (req, res) => {
+    console.log(JSON.stringify(req.body));
+    const newTask = new taskModel({
+        _id: new mongoose.Types.ObjectId, //req.params.id,
+        taskName: req.body.taskName,
+        predictedEndDate: req.body.deadline,
+        priority: req.body.priority,
+        creator: req.params.id,
+    });
+
+    console.log(newTask);
+    console.log(newTask.taskName);
+
+    const user = await userModel.find({_id: req.params.id});
+    console.log(user);
+    user.tasks.push(newTask);
+    //user.tasks = user.tasks.push(newTask);
     // check if req.body is formatted correctly
 
     try {
-        await user.save();
+        await this.user.save((e,u) => { console.log('New task saved to user!'); });
         res.send(user);
     } catch (error) {
         res.status(500).send(error);

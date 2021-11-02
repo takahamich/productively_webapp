55eacc99f18695f4856fdc24d3ec6bda14cceed2
Ayo Ajayi <ayomide.ajayi2839@gmail.com>
2021-11-02 16:12:40 -0400

    Added functions to get specific data fields from the database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-//const mongoose = require("mongoose");
-//const Router = require("./routes");
+const mongoose = require("mongoose");
+const Router = require("./routes");
 const bodyParser = require('body-parser');
-
+const taskModel = require("./models/Task");
+const userModel = require("./models/User");
 const cors = require('cors')
 const app = express();
 const port = 8080;
@@ -15,14 +16,16 @@ app.get('/', (req, res) => {
     res.send({ express: 'React connected to Express back-end' });
 });
 
-app.post('/tasks', (req, res) => {
+app.post('/', (req, res) => {
     console.log(req.body);
-    console.log(`I received your POST request. This is what you sent me: ${req.body}`);
-    res.send(req.body.taskName);
+    console.log(`I received your POST request. This is what you sent me ${req.body}`);
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-/*
+
+
+
 mongoose.connect(
     `mongodb+srv://nolombardo:%40ndw3simplys%40id@cluster0.kjv7f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
 );
@@ -30,6 +33,28 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
     console.log("Connected successfully");
+    console.log(`Tasks:`);
+    taskModel.find({}, function (err, result) {
+        if (err) return handleError(err);
+        // console.log(result);
+
+        for(var i = 0; i < result.length; i++) {
+            var obj = result[i];
+            myFunction(obj.startTime, obj.endTime);
+        }
+    })
+    
 });
+function myFunction(startTimeValue, endTimeValue) {
+    console.log("IN HERE", startTimeValue, endTimeValue);
+    return startTimeValue, endTimeValue  // The function returns the product of p1 and p2
+  }
+
+
+
 app.use(Router);
-//TEST BRANCH*/
+
+
+
+//TEST BRANCH
+

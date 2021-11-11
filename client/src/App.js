b1dd6d011f925daa5d97de597ce4ed3b69585321
Shaina Gabala <sgabala2@jh.edu>
2021-11-10 20:22:47 -0500

    Google Login Implemented. tasks are being assigned to users as well

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,166 @@
 import React from 'react';
 import './App.css'
-import {useState } from "react";
+import {useEffect, useState} from "react";
 import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
-import Login from './components/Login';
 import 'react-calendar/dist/Calendar.css';
 import Calendars from './components/Calendars.js';
 import styled from 'styled-components'
 import TaskButton from "./components/TaskButton";
 import Task from "./components/Task";
+import { loadGoogleScript } from './GoogleLogin';
+import logo from './logo.svg'
+const googleClientId = '844612426523-iqc51n1du6su4dome75g0n7p35ru5k7j.apps.googleusercontent.com'
 
+export let creator = "";
 
 function App() {
+    const [toggle, setToggle] = useState(false);
 
-  //const [token, setToken] = useState();
-  const [toggle, setToggle] = useState(false);
-/*
-  if(!token) {
-    return <Login setToken={setToken} />
-  }*/
+    //LOGIN FUNCTIONALITY
+    const [gapi, setGapi] = useState();
+    const [googleAuth, setGoogleAuth] = useState();
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [name, setName] = useState('');
+    const [userId, setId] = useState(); //might be able to pass this in to post tasks to specific user
+    const [userEmail, setEmail] = useState('');
 
+    creator = userEmail;
+
+    const onSuccess = (googleUser) => { // (Ref. 7)
+        setIsLoggedIn(true);
+        const profile = googleUser.getBasicProfile();
+        setName(profile.getName());
+        setId(profile.getId());
+        setEmail(profile.getEmail());
+
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+
+        // The ID token you need to pass to your backend:
+        const id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+
+        const newUser = {
+            googleId: id_token,
+            name: profile.getName(),
+            email: profile.getEmail(),
+        }
+
+        fetch('http://localhost:8080/signedin', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            console.log(response.text);
+        }).catch(err => {
+            console.log(err);
+        });
+
+
+        alert("Your profile info has successfully been posted!");
+    };
+
+    const onFailure = () => {
+        setIsLoggedIn(false);
+    }
+
+    const logOut = () => { // (Ref. 8)
+        (async() => {
+            await googleAuth.signOut();
+            setIsLoggedIn(false);
+            renderSigninButton(gapi);
+        })();
+    };
+
+    const renderSigninButton = (_gapi) => { // (Ref. 6)
+        _gapi.signin2.render('google-signin', {
+            'scope': 'profile email',
+            'width': 240,
+            'height': 50,
+            'longtitle': true,
+            'theme': 'dark',
+            'onsuccess': onSuccess,
+            'onfailure': onFailure
+        });
+    }
+
+
+    useEffect(() => {
+
+        // Window.gapi is available at this point
+        window.onGoogleScriptLoad = () => { // (Ref. 1)
+
+            const _gapi = window.gapi; // (Ref. 2)
+            setGapi(_gapi);
+
+            _gapi.load('auth2', () => { // (Ref. 3)
+                (async () => {
+                    const _googleAuth = await _gapi.auth2.init({ // (Ref. 4)
+                        client_id: googleClientId
+                    });
+                    setGoogleAuth(_googleAuth); // (Ref. 5)
+                    renderSigninButton(_gapi); // (Ref. 6)
+                })();
+            });
+        }
+
+        // Ensure everything is set before loading the script
+        loadGoogleScript(); // (Ref. 9)
+
+    }, []);
+    //END
   // Handle when the user creates the create task button
   function handleOnClick(){
-    setToggle(!toggle);
+      setToggle(!toggle);
   }
 
+
   return (
-  <ParentWrapper>
-    <h1>Welcome Home, User</h1>
-    <SubParentWrapper selected={toggle}>
-      <TopWrapper>
-        <CalendarWrapper>
-          <Calendars/>
-        </CalendarWrapper>
-        <ButtonWrapper>
-          <TaskButton onClick={handleOnClick}/>
-        </ButtonWrapper>
-        <ButtonWrapper>
-          <button>View my Tasks</button>
-        </ButtonWrapper>
-      </TopWrapper>
-      {/* <BodyWrapper>
-        <CalendarWrapper> 
-          <Calendar/>
-        </CalendarWrapper>
-        <TaskWrapper/>
-      </BodyWrapper> */}
-    
-    </SubParentWrapper>
-    {toggle && <Task onClick={handleOnClick}/>}
-  </ParentWrapper>
+      <div className="App">
+          <div className="head">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h2>'App Name'</h2>
+              <h3>the best time-management web application</h3>
+          </div>
+          {!isLoggedIn &&
+          <div id="google-signin"></div>
+          }
+
+          {isLoggedIn &&
+              <ParentWrapper>
+                  <h1>Welcome Home, {name}</h1>
+                  <button className='btn-primary' onClick={logOut}>Log Out</button>
+                  <SubParentWrapper selected={toggle}>
+                      <TopWrapper>
+                        <CalendarWrapper>
+                            <Calendars/>
+                        </CalendarWrapper>
+                          <ButtonWrapper>
+                              <TaskButton onClick={handleOnClick}/>
+                          </ButtonWrapper>
+                          <ButtonWrapper>
+                              <button>View my Tasks</button>
+                          </ButtonWrapper>
+                      </TopWrapper>
+                  {/*<BodyWrapper>
+                    <CalendarWrapper>
+                    <Calendar/>
+                    </CalendarWrapper>
+                    <TaskWrapper/>
+                    </BodyWrapper> */}
+                  </SubParentWrapper>
+                  {toggle && <Task onClick={handleOnClick}/>}
+              </ParentWrapper>
+              }
+
+      </div>
   )
 }
 
--- server/routes.js
+++ server/routes.js
@@ -33,10 +33,43 @@ app.get("/tasks", async (req, res) => { //gets all tasks
     }
 });
 
+app.post('/signedin', (req, res) => {
+    console.log(req.body);
+
+    userModel.findOne({email: req.body.email}, (err,user)=> {
+        if (err) {
+            throw new Error("Error finding this user");
+        }
+
+        if(!user) {
+            console.log('Could not find you in the database, Making a new user!');
+            const newUser = new userModel ({
+                _id: new mongoose.Types.ObjectId,
+                googleId: req.body.googleId,
+                name: req.body.name,
+                email: req.body.email,
+            });
+            console.log(newUser);
+            userModel.create(newUser, (err, user) => {
+                if (err) {
+                    res.redirect('/');
+                    console.log("Error while creating new user");
+                    throw new Error(err);
+                }
+            });
+        } else {
+            console.log('User was already in our database!');
+            res.send(user);
+        }
+    });
+
+});
+
 app.post('/tasks', (req, res) => {
     console.log(req.body);
-    const newTask = new taskModel ({
+    const newTask = new taskModel({
         _id: new mongoose.Types.ObjectId, //req.params.id,
+        creator: req.body.creatorId,
         taskName: req.body.taskName,
         startDate: req.body.startDate,
         status: req.body.status,
@@ -49,30 +82,50 @@ app.post('/tasks', (req, res) => {
         endTime: req.body.end,
     });
 
-    taskModel.create(newTask, (err, task) => {
+    userModel.findOne({email: newTask.creator}, (err,user)=> {
         if (err) {
-            res.redirect('/');
-            throw new Error("error when posting task");
+            throw new Error(err);
         }
+
+        console.log("found user " + user.name + ". We will be adding following task ");
+        console.log(newTask);
+
+        //task model.create
+        newTask.save((err) => {
+            if (err) {
+                // res.redirect('/');
+                alert("error when posting task, please try again");
+                throw new Error("error when posting task");
+            }
+        });
+
+        user.tasks.push(newTask);
+        console.log('the user task array has been updated ' + user.tasks);
+        user.save((err) => {
+            res.send(user.tasks);
+        });
     });
 
-    console.log(`I received your POST request. This is what you sent me`);
+    console.log("I received your POST request. This is what you sent me");
     console.log(newTask);
 
-    var prodScore = Number(req.body.ActualTime) / Number(req.body.PredictedTime);
+    let prodScore = Number(req.body.ActualTime) / Number(req.body.PredictedTime);
     switch (true) {
         case (prodScore < .75):
             console.log("Your productivity score is: " + prodScore + "." +
                 "Wow! You're hyper-productive. " +
                 "Feel free to do more things, or just enjoy your day!");
+            break;
         case (prodScore <= 1):
             console.log("Your productivity score is: " + prodScore + "." +
                 "Yay! You're pretty spot on with your time estimates. " +
                 "Keep it up!");
+            break;
         case (prodScore < 1.5):
             console.log("Your productivity score is: " + prodScore + "." +
                 "Hmm, do you want to add some buffer time in your day, " +
                 "and plan spend more time on your tasks?");
+            break;
         case (prodScore >= 1.5):
             console.log("Your productivity score is: " + prodScore + "." +
                 "Oof. Do you need a day off on [day of the week]s? " +
@@ -85,19 +138,23 @@ app.post('/tasks', (req, res) => {
                 case (startHour < 12):
                     console.log("Hmm. Maybe you're not a morning person. " +
                         "What do you think about not assigning yourself tasks during the morning?");
+                    break;
                 case (startHour < 17) :
                     console.log("Hmm. Maybe you're not a afternoon person. " +
                         "What do you think about not assigning yourself tasks during the afternoon?");
+                    break;
                 case (startHour < 21):
                     console.log("Hmm. Maybe you're not a evening person. " +
                         "What do you think about not assigning yourself tasks during the evening?");
+                    break;
                 case (startHour >= 21):
                     console.log("Hmm. Maybe you're not a night person. " +
                         "What do you think about not assigning yourself tasks during the night?");
+                    break;
             }
     }
 
-    res.send(newTask);
+    //res.send(newTask);
 });
 
 // app.get("/tasks/:id", async (req, res) => {

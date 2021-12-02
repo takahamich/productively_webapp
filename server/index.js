f675ac2ecd42004ade9b3b9e66c43cb78372f2df
Octobserver <77016782+Octobserver@users.noreply.github.com>
2021-12-02 13:27:19 -0500

    Add schedule endpoint

--- server/index.js
+++ server/index.js
@@ -32,82 +32,99 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
     console.log("Connected successfully");
-    //console.log(`Tasks:`);
-    /*taskModel.find({}, function (err, result) {
-        if (err) return handleError(err);
-        // console.log(result);
 
-        for(var i = 0; i < result.length; i++) {
-            var obj = result[i];
-            myFunction(obj.startTime, obj.endTime);
-        }
-    })*/
-    Schedule();
+    //Schedule();
     
 });
 /*function myFunction(startTimeValue, endTimeValue) {
     console.log("IN HERE", startTimeValue, endTimeValue);
     return startTimeValue, endTimeValue  // The function returns the product of p1 and p2
   }*/
+let message = [];
+
+app.post('/tasks/schedule', (req, res) => {
+    let id = JSON.stringify(req.body);
+    console.log("User email:" + id);
+    console.log("Handling POST request for schedule...");
+    message = Schedule(id);
+    console.log(message);
+    res.send(message);
+})
+
+
+/*app.get('/tasks/schedule', (req, res) => {
+    try {
+        console.log("Handling GET request for schedule...");
+        console.log(message);
+        res.send(message);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});*/
+
+
 
 // Scheduling Algorithm
-function Schedule(){
-    taskModel.find({}, function (err, result) {
-        if (err) return handleError(err);
-
-        let tasks = [];
-        for(var i = 0; i < result.length; i++) {
-            let obj = result[i];
+function Schedule(id = ''){
+    let result = findTasks(id);
+    let message = [];
+    let tasks = [];
+    for(var i = 0; i < result.length; i++) {
+        let obj = result[i];
             //The parameter value is the weight assigned to each task.
             // Time-consuming tasks with high priority have larger weights.
             // We will take difficulty level into consideration in the next iteration.
-            tasks.push({id:obj.id,
-                taskName: obj.taskName,
-                deadline: new Date(obj.predictedEndDate),
-                predTime:parseInt(obj.predictedTime),
-                value:parseInt(obj.priority) * parseInt(obj.predictedTime)})
-        }
-        
-        tasks.sort(compareDeadline);
-        //console.log("Sorted Tasks with Weights")
-        //console.log(tasks)
+        tasks.push({id:obj.id,
+            taskName: obj.taskName,
+            deadline: new Date(obj.predictedEndDate),
+            predTime:parseInt(obj.predictedTime),
+            value:parseInt(obj.priority) * parseInt(obj.predictedTime)})
+    }
 
-        //Prompt a schedule for the user
-        let today = new Date(); // This is based on UTC, will consider change timezone to EST next iteration.
-        let tasksDueToday = [];
-        let tasksToStart = [];
-        for(var i = 0; i < tasks.length; i++) {
-            if (tasks[i].deadline.getDate() - today.getDate() == 1) {
-                tasksDueToday.push(tasks[i])
+    tasks.sort(compareDeadline);
+    //console.log("Sorted Tasks with Weights")
+    //console.log(tasks)
+
+    //Prompt a schedule for the user
+    let today = new Date(); // This is based on UTC, will consider change timezone to EST next iteration.
+    let tasksDueToday = [];
+    let tasksToStart = [];
+    for(var i = 0; i < tasks.length; i++) {
+        if (tasks[i].deadline.getDate() - today.getDate() == 1) {
+            tasksDueToday.push(tasks[i])
+        }
+        //Giving a task two buffer days before the deadline
+        else if (tasks[i].deadline.getDate() - today.getDate() < 2) {
+            tasksToStart.push(tasks[i])
+        }
+    }
+    if (tasksDueToday.length == 0) {
+        message.push("Yay! You do not have any task due today, but you can start early on other tasks.");
+        message.push("Here is a list of tasks to start:");
+        createTodaysSchedule(tasksToStart, message)
+    }
+    else {
+        message.push("There are tasks that are due today! Take a look before it is too late!")
+        let i = createTodaysSchedule(tasksDueToday, message)
+        if (i >= 0) {
+            message.push("Oh no! It seems that you cannot finish all tasks that are due today.");
+            message.push("We suggest you leave out these less important tasks:")
+            for(var j = 0; i < tasksDueToday.length; i++) {
+                message.push(tasksDueToday[j].taskName);
             }
-            //Giving a task two buffer days before the deadline
-            else if (tasks[i].deadline.getDate() - today.getDate() < 2) {
-                tasksToStart.push(tasks[i])
-            }
-        }
-        if (tasksDueToday.length == 0) {
-            console.log("Yay! You do not have any task due today, but you can start early on other tasks.")
-            console.log("Here is a list of tasks to start:")
-            createTodaysSchedule(tasksToStart)
-        }
-        else {
-            console.log("There are tasks that are due today! Take a look before it is too late!")
-            let i = createTodaysSchedule(tasksDueToday)
-            if (i >= 0) {
-                console.log("Oh no! It seems that you cannot finish all tasks that are due today.");
-                console.log("We suggest you leave out these less important tasks:")
-                for(var j = 0; i < tasksDueToday.length; i++) {
-                    console.log(tasksDueToday[j]);
-                }
-            }
-
         }
 
+    }
+    return message;
 
-
-    })
 
 }
+
+const findTasks = async function(id) {
+    try {  return await taskModel.find({})
+    } catch(err) { console.log(err) }
+}
+
 
 function compareDeadline( task1, task2 ) {
     //assume the predictedEndTime is in the format of YYYY-MM-DD
@@ -134,20 +151,20 @@ function compareWeight( task1, task2 ) {
     return 0;
 }
 
-function createTodaysSchedule(taskList) {
+function createTodaysSchedule(taskList, message) {
     let start = new Date();
     //In the next iteration, it may be better to let users to input their free time
     //Currently, assume the user has a whole day to work on the tasks (9 am to 11 pm)
     taskList.sort(compareWeight);
     let currHour = 9;
-    console.log("Today's schedule:")
+    message.push("Today's schedule:");
     for(var i = 0; i < taskList.length; i++) {
         if (currHour > 23) {
             return i;
         }
         start.setHours(currHour);
-        console.log("Start time: " + start.getHours() + ":00");
-        console.log("Task Name: " + taskList[i].taskName);
+        message.push("Start time: " + start.getHours() + ":00");
+        message.push("Task Name: " + taskList[i].taskName) ;
         currHour =  currHour + taskList[i].predTime + 1;
     }
     return -1;
@@ -178,4 +195,3 @@ app.use(Router);
 
 
 //TEST BRANCH
-

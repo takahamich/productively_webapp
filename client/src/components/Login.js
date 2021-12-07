bf571af08f2ba6dbbb4a10f19c448287f140ff33
Shaina Gabala <sgabala2@jh.edu>
2021-12-06 21:58:08 -0500

    final design for log in page, just missing logo

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Login.css';
 import time from '../time.png'
+import styled from "styled-components";
 
 function Login() {
     const googleLogin = () => { //was const not function = () => {
@@ -9,41 +10,43 @@ function Login() {
     }
 
     return(
-        // <div className="login-wrapper">
-        //     <div className="login-headers">
-        //         <h1>Hi! It's nice to see you here!</h1>
-        //         <h2>please log in to access web-app.</h2>
-        //         <div className="googleContainer" onClick={googleLogin}>
-        //             <img src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png' alt="Google Icon" />
-        //             <p>Login With Google</p>
-        //         </div>
-        //         {/*<LoginButton/>*/}
-        //     </div>
-        // </div>
-
-    <div className="main">
-        <div className="sub-main">
-            <div>
-                <div className="imgs">
-                    <div className="container-image">
-                        <img src={time} alt="temp" className="profile"/>
-                    </div>
-                </div>
-                <div>
-                    <h1>Welcome to Productivity!</h1>
-                    <div className="googleContainer" onClick={googleLogin}>
-                        <img src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png' alt="Google Icon" />
-                        <p>Login With Google</p>
-                    </div>
+        <Main>
+            <div className="center">
+                <h1 style={textStyle}>Welcome!</h1>
+                <h3 style={smallTextStyle}>Please Sign In With Your Google Account </h3>
+                <h3 style={smallTextStyle}>To Access Your Dashboard </h3>
+                <div className="button" onClick={googleLogin}>
+                    <img src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-suite-everything-you-need-know-about-google-newest-0.png' alt="Google Icon" />
+                    <p>Login With Google</p>
                 </div>
             </div>
-
-        </div>
-    </div>
+        </Main>
 
     )
 }
 
 
 export default Login;
+
+const Main = styled.div`
+    height: 100vh;
+    width: 100vw;
+    background: #377F87;
+    font-family: 'Proxima Nova';
+    text-transform: uppercase;
+    text-align: center;
+    font-size: 1em;
+    display: flex;
+    flex-flow: column;
+`
+
+const textStyle = {
+    textDecoration: 'none',
+    color: '#F6F6F2'
+}
+
+const smallTextStyle = {
+    textDecoration: 'none',
+    color: '#BADFE7'
+}
 
